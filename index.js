96cf15fe501520c5ef6ae0c9c09658e938901f36
b000rg <33711099+b000rg@users.noreply.github.com>
2020-07-14 20:14:16 -0500

    completed Car class definition

--- index.js
+++ index.js
@@ -75,8 +75,29 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  };
 
-}
+  fill(gallons) {
+    this.tank += gallons;
+  };
+
+  drive(distance) {
+    let maxDistance = this.tank * this.milesPerGallon;
+    if (distance <= maxDistance) {
+      this.odometer += distance;
+      this.tank -= distance / this.milesPerGallon;
+    } else {
+      this.tank = 0;
+      this.odometer += maxDistance;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  };
+};
 
 /*
   TASK 3
